dd3dee62aa0edf66eabca3164db3de599c05d7b4
Alan Strauhs <alan.strauhs@wearesauce.io>
2023-09-06 21:09:39 +0100

    feat: api interface for floatrates

--- src/api/floatrates.js
+++ src/api/floatrates.js
@@ -1,16 +1,49 @@
 import axios from 'axios';
+import {currencies} from 'constants/currencies';
 
-export const currencies = ['usd', 'gbp', 'cad', 'aud', 'jpy'];
 
 function apiInstance() {
   return axios.create({
     baseURL: 'http://www.floatrates.com/daily/',
-    timeout: 1000,
+    timeout: 5000,
+    transformResponse: [
+      (data) => {
+        let conversionRates = JSON.parse(data);
+        Object.keys(conversionRates).forEach((currency) => {
+          const currencyRate = conversionRates[currency];
+          
+          conversionRates[currency] = {
+            rate: currencyRate["rate"],
+            date: currencyRate["date"]
+          }
+        })
+
+        return conversionRates;
+      }
+    ]
   });
 }
 
-export function requestExchangeRate(currency) {
+function getEndpoint(currency) {
+  return `${currency}.json`;
+}
+
+export function requestAllExchangeRates() {
   const instance = apiInstance();
-  const endpoint = `/${currency}.json`;
-  return instance.get(endpoint)
+  console.log("ran")
+  return Promise.all(currencies.map(async (currency) => {
+    return [currency, (await requestExchangeRate(currency, instance)).data];
+  })).then(
+    (data) => {
+      return Object.fromEntries(data);
+    },
+    (error) => {
+      return error;
+    }
+  );
+}
+
+export function requestExchangeRate(currency, instance = apiInstance()) {
+  const endpoint = getEndpoint(currency);
+  return instance.get(endpoint);
 }
