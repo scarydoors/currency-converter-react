3798e35eb6e8b110b80c59c1bd22e1da2ea8987d
Alan Strauhs <alan.strauhs@wearesauce.io>
2023-09-06 21:10:21 +0100

    fix: syntax error

--- src/components/ui/Card.js
+++ src/components/ui/Card.js
@@ -1,6 +1,6 @@
 export default function Card({children}) {
   return (
-    <div class="grow max-w-lg p-6 rounded-lg border border-gray-200 shadow-md bg-white">
+    <div className="grow max-w-lg p-6 rounded-lg border border-gray-200 shadow-md bg-white">
       {children}
     </div>
   )
