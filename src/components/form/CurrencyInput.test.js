e8e3ce7e366c81b9200d3b78d3537531ed5a9446
Alan Strauhs <alan.strauhs@wearesauce.io>
2023-09-07 14:42:19 +0100

    chore: create tests for CurrencyInput

---
+++ src/components/form/CurrencyInput.test.js
@@ -0,0 +1,39 @@
+import {render, screen, act} from '@testing-library/react';
+import '@testing-library/jest-dom';
+import CurrencyInput from './CurrencyInput';
+
+describe("<CurrencyInput /> validations", () => {
+  const id = "currencyInput";
+  const onChange = jest.fn();
+  const label = "Amount";
+  const amountRequiredMessage = "A valid amount is required";
+
+  const setup = async (value) => {
+    return act(() => {
+      render(
+        <CurrencyInput id={id} label={label} onChange={onChange} value={value}/>
+      )
+    });
+  }
+
+  test('amount required validation works', async () => {
+    await setup({currency: "aud", amount: ""});
+    expect(screen.queryByText(amountRequiredMessage)).toBeInTheDocument();
+  })
+
+  test('amount min validation works', async () => {
+    await setup({currency: "aud", amount: "-2.00"});
+    expect(screen.queryByText(amountRequiredMessage)).toBeInTheDocument();
+  })
+
+  test('amount scientific number validation works', async () => {
+    await setup({currency: "aud", amount: "2.002343234234234e+24"});
+    expect(screen.queryByText("Amount must be to 2 decimal places")).toBeNull();
+  })
+
+  test('amount valid value validation works', async () => {
+    await setup({currency: "aud", amount: "2.00"});
+    expect(screen.queryByRole("alert")).toBeNull();
+  })
+
+})
