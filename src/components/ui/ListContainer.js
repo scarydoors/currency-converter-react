99a93aa0c69dccafd141d35e2e55644156a2d8bc
Alan Strauhs <alan.strauhs@wearesauce.io>
2023-09-07 20:12:20 +0100

    fix: use of reserved keywords

--- src/components/ui/ListContainer.js
+++ src/components/ui/ListContainer.js
@@ -11,11 +11,11 @@ import { Children } from 'react';
 export default function ListContainer({ children, loadingRender, loading, errorRender, error }) {
   const childrenList = Children.toArray(children);
   return (
-    <div class="absolute inset-0 overflow-none flex flex-col justify-end md:justify-center items-center space-y-4 bg-gray-50 p-6">
+    <div className="absolute inset-0 overflow-none flex flex-col justify-end md:justify-center items-center space-y-4 bg-gray-50 p-6">
       {loading && loadingRender}
       {error && errorRender}
       {!(loading || error) &&
-       childrenList.map((child, idx) => <div key={idx} class="flex flex-row w-full justify-center">{child}</div>)}
+       childrenList.map((child, idx) => <div key={idx} className="flex flex-row w-full justify-center">{child}</div>)}
     </div>
   );
 }
