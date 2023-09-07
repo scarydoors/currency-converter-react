8119a08d9348aa21176f8c69065a9ec26c0c3a6b
Alan Strauhs <alan.strauhs@wearesauce.io>
2023-09-07 20:07:48 +0100

    fix: missing key when mapping children components

--- src/components/ui/ListContainer.js
+++ src/components/ui/ListContainer.js
@@ -15,7 +15,7 @@ export default function ListContainer({ children, loadingRender, loading, errorR
       {loading && loadingRender}
       {error && errorRender}
       {!(loading || error) &&
-        childrenList.map((child) => <div class="flex flex-row w-full justify-center">{child}</div>)}
+       childrenList.map((child, idx) => <div key={idx} class="flex flex-row w-full justify-center">{child}</div>)}
     </div>
   );
 }
