dd81a1419bbfb64092e03a676f2b570675759954
Alan Strauhs <alan.strauhs@wearesauce.io>
2023-09-09 22:25:44 +0100

    refactor: no longer need to handle error state in App.js

--- src/App.js
+++ src/App.js
@@ -3,21 +3,7 @@ import UI from 'components/ui';
 
 function App() {
   return (
-    <UI.ListContainer
-      loading={false}
-      loadingRender={
-        <div className="text-center space-y-6 p-6">
-          <UI.Spinner className="w-16 h-16" />
-          <p>Loading Conversion Rates...</p>
-        </div>
-      }
-      error={false}
-      errorRender={
-        <div className="text-center space-y-6 p-6">
-          <p>Could not retrieve the exchange rates. Please check your internet connection.</p>
-        </div>
-      }
-    >
+    <UI.ListContainer>
       <UI.Card>
         <h1 className="text-2xl font-medium mb-2">Currency Converter</h1>
         <CurrencyConversionForm />
